e4e73a31f7794a116f94a9e08c4cb311003ed0c0
tejasra <56512902+tejasra@users.noreply.github.com>
2021-01-05 12:50:45 +0530

    The server is update with the actual code.

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log("This is start...");
+const readline = require("readline");
+var assert = require("assert");
+
+const getInput = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const getAngle = (hour, minute) => {
+  return 360 - Math.abs(hour * 30 + minute * -5.5);
+};
+
+getInput.question(`Enter the hour  `, (hour) => {
+  getInput.question(`Enter the minute  `, (minute) => {
+    assert(
+      hour <= 12 && minute <= 60,
+      "Error!! The hour should be less than 12 and minute should be less than 60"
+    );
+    console.log(
+      `The angle between ${hour} and ${minute} is ${getAngle(hour, minute)}`
+    );
+    getInput.close();
+  });
+});
